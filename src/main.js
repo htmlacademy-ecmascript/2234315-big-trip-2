7ee99b659c089d7dcfdf6900ed60cfd0f69ef3be
katya-illarionova <katya.illarionova@travelline.ru>
2024-04-24 22:41:53 +0300

    Подключит модель

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import FilterView from './view/filter-view.js';
 import AppPresenter from './presenter/app-presenter.js';
-import {render} from './render.js';
+import PointModel from './model/point-model.js';
 
-const filterWrapper = document.querySelector('.trip-controls__filters');
-const appPresenter = new AppPresenter();
+const pointsListWrapper = document.querySelector('.trip-events');
 
-render(new FilterView(), filterWrapper);
+const pointModel = new PointModel();
+const appPresenter = new AppPresenter({
+  pointsListContainer: pointsListWrapper,
+  pointModel
+});
+
 appPresenter.init();
--- src/presenter/app-presenter.js
+++ src/presenter/app-presenter.js
@@ -1,3 +1,4 @@
+import FilterView from '../view/filter-view.js';
 import SortView from '../view/sort-view.js';
 import PointsListView from '../view/points-list-view.js';
 import PointsListItemView from '../view/points-list-item-view.js';
@@ -5,22 +6,30 @@ import PointEditView from '../view/point-edit-view.js';
 import PointView from '../view/point-view.js';
 import {render} from '../render.js';
 
-const pointsListWrapper = document.querySelector('.trip-events');
+const filterWrapper = document.querySelector('.trip-controls__filters');
 
 export default class AppPresenter {
   pointsListComponent = new PointsListView();
   pointEditFormComponent = new PointsListItemView();
 
+  constructor({pointsListContainer, pointModel}) {
+    this.pointsListContainer = pointsListContainer;
+    this.pointModel = pointModel;
+  }
+
   init() {
-    render(new SortView, pointsListWrapper);
-    render(this.pointsListComponent, pointsListWrapper);
+    this.points = [...this.pointModel.getPoints()];
+
+    render(new FilterView, filterWrapper);
+    render(new SortView, this.pointsListContainer);
+    render(this.pointsListComponent, this.pointsListContainer);
     render(this.pointEditFormComponent, this.pointsListComponent.getElement());
     render(new PointEditView, this.pointEditFormComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < this.points.length; i++) {
       const pointsListItemComponent = new PointsListItemView();
       render(pointsListItemComponent, this.pointsListComponent.getElement());
-      render(new PointView, pointsListItemComponent.getElement());
+      render(new PointView ({point: this.points[i]}), pointsListItemComponent.getElement());
     }
   }
 }
