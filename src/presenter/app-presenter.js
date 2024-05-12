e3415ae861493d55ab9c3355a429aba1bb970774
katya-illarionova <katya.illarionova@travelline.ru>
2024-05-13 00:12:55 +0300

    Добавит классам приватные поля и геттеры

--- src/presenter/app-presenter.js
+++ src/presenter/app-presenter.js
@@ -6,36 +6,43 @@ import PointView from '../view/point-view.js';
 import { render } from '../framework/render.js';
 
 export default class AppPresenter {
-  pointsListComponent = new PointsListView();
-  pointEditFormComponent = new PointsListItemView();
+  #pointsListContainer = null;
+  #pointModel = null;
+
+  #pointsListComponent = new PointsListView();
+  #pointEditFormComponent = new PointsListItemView();
+
+  #points = [];
+  #offers = [];
+  #destinations = [];
 
   constructor({ pointsListContainer, pointModel }) {
-    this.pointsListContainer = pointsListContainer;
-    this.pointModel = pointModel;
+    this.#pointsListContainer = pointsListContainer;
+    this.#pointModel = pointModel;
   }
 
   init() {
-    this.points = [...this.pointModel.getPoints()];
-    this.offers = [...this.pointModel.getOffers()];
-    this.destinations = [...this.pointModel.getDestinations()];
+    this.#points = [...this.#pointModel.points];
+    this.#offers = [...this.#pointModel.offers];
+    this.#destinations = [...this.#pointModel.destinations];
 
-    render(new SortView, this.pointsListContainer);
-    render(this.pointsListComponent, this.pointsListContainer);
-    render(this.pointEditFormComponent, this.pointsListComponent.element);
+    render(new SortView, this.#pointsListContainer);
+    render(this.#pointsListComponent, this.#pointsListContainer);
+    render(this.#pointEditFormComponent, this.#pointsListComponent.element);
     render(new PointEditView({
-      point: this.points[0],
-      offers: this.offers,
-      destinations: this.destinations
-    }), this.pointEditFormComponent.element);
+      point: this.#points[0],
+      offers: this.#offers,
+      destinations: this.#destinations
+    }), this.#pointEditFormComponent.element);
 
-    for (let i = 1; i < this.points.length; i++) {
+    for (let i = 1; i < this.#points.length; i++) {
       const pointsListItemComponent = new PointsListItemView();
 
-      render(pointsListItemComponent, this.pointsListComponent.element);
+      render(pointsListItemComponent, this.#pointsListComponent.element);
       render(new PointView({
-        point: this.points[i],
-        offers: this.offers,
-        destinations: this.destinations
+        point: this.#points[i],
+        offers: this.#offers,
+        destinations: this.#destinations
       }), pointsListItemComponent.element);
     }
   }
